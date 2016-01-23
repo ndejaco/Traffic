88b67d35e35f6bbfa8c797ccf92eec5eff1d79c7
Youhao Wei <youhaowei@email.arizona.edu>
2016-01-23 15:13:28 -0700

    Add missing ":"

--- removeStall.py
+++ removeStall.py
@@ -7,10 +7,10 @@ if len(sys.argv) == 3:
 elif len(sys.argv) == 1:
 	masterFile = sys.stdin
 	inFile = sys.stdout
-else
-	print 'Error: give an input and output file as arguments'
-	print '       or pipe in/out of this program'
-	sys.exit(1)
+else:
+    print 'Error: give an input and output file as arguments'
+    print '       or pipe in/out of this program'
+    sys.exit(1)
 
 lastLatLong = ""
 line = inFile.readline()
@@ -22,5 +22,5 @@ while line:
    if latLong != lastLatLong:
        masterFile.write(line)
        lastLatLong = latLong
-   line = inFile.readline() 
-    
+   line = inFile.readline()
+
