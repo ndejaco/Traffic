f0e65c8570b4d816a2860d2bb0d374dcc4a6705b
Youhao Wei <youhaowei@email.arizona.edu>
2016-01-23 18:46:12 -0700

    Fix vis and index.html

--- vis.js
+++ vis.js
@@ -2,7 +2,7 @@ var startTime = new Date().getTime();
 var map = L.map('map').setView([32.22, -110.93], 11);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 25,
     id: 'youhaowei.op72jf9g',
     accessToken: 'pk.eyJ1IjoieW91aGFvd2VpIiwiYSI6ImNpanFyOTl0ZTAzcnZ1eWx4M3R4YWh4cjkifQ.euhHOxoz-qgt9e93YGqlSQ'
 }).addTo(map);
@@ -11,45 +11,53 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 //var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
 var colorScale = d3.scale.category20();
 var i = 0;
-d3.csv("tr_all_3col_1M.csv", function(error, data){
+d3.csv("tr_all_filt_7col.csv", function(error, data){
     console.log("file loaded: " + (new Date().getTime() - startTime) + " ms.")
     if (error) {
         alert(error);
     }
     console.log(data.length)
-    data = data.filter(function(d,i){ return i % 1 === 0;})
+    data = data.filter(function(d,i){ return i % 2 === 0;})
     console.log(data.length)
     //console.log(data);
-    var paths = {};
+    var paths = [];
+    var currPath = {tid: -1};
     for (var d in data) {
         d = data[d];
         //console.log(d);
         //console.log(d.LocationLatitude);
         //console.log(d.LocationLongitute);
         var l = L.latLng(d.LocationLatitude,d.LocationLongitute)
+
         //console.log(l);
-        if (paths[d.TripID]) {
-            paths[d.TripID].push(l);
+        if (currPath.tid == d.TripID) {
+            //console.log("Hello");
+            currPath.path.push(l)
         } else {
-            paths[d.TripID] = [l];
+            //console.log("World!");
+            if (currPath.tid >= 0) {
+                //console.log(currPath.tid);
+                paths.push(currPath);
+            }
+            var date = new Date(d.unixtime);
+            currPath = {tid: d.TripID, date: date, path: [l]};
         }
     }
+    delete data;
     console.log("Data Loop Done: " + (new Date().getTime() - startTime) + " ms.")
-    polylines = [];
+    //console.log(paths)
     for (var p in paths) {
         //console.log(paths[p]);
-        var polyline = L.polyline(paths[p], {color: "#eee", weight: "2", opacity:"0.05", className: "t" + p}).addTo(map);
-        polylines.push(polyline);
+        var polyline = L.polyline(paths[p].path, {color: "#fff", opacity: 0.05, weight: 2, className: "t" + p}).addTo(map);
+        //polylines.push(polyline);
         polyline.on("click", function(e) {
-            e.target.setStyle({ color: colorScale(i), weight: "5", opacity:"0.75" });
+            e.target.setStyle({ color: colorScale(i), opacity: 0.75, weight:6});
             e.target.bringToFront();
-            console.log(e.target._path);
             i++;
         })
-
     }
+    console.log(paths);
     console.log("All Done: " + (new Date().getTime() - startTime) + " ms.")
-
 }
 /*, function(error, rows) {
     console.log(error + ": " + rows);
